d4bee60c1188d49acb20096322c72669659bb4cc
Rintaro0726 <79673107+Rintaro0726@users.noreply.github.com>
2021-07-31 02:44:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
+const animation = document.querySelectorAll('.js-trigger');
+const options = {
+  root: null,
+  rootMargin: "-30% 0px",
+  threshold: 0
+};
+animation.forEach((target) => this.onIntersect(target, options));
 
-$(function(){
-  $(window).on('scroll',function (){
-    let triggerClass = $('.js-trigger');       //トリガーとなるクラス
-        animateClass = ('is-show');
- 
-    $(triggerClass).each(function(){
-      let scroll       = $(window).scrollTop(), //スクロール位置を取得
-          triggerTop   = $(this).offset().top,  //指定要素のY座標
-          windowHeight = $(window).height();    //ウィンドウの高さ
-      // 要素が画面中央に来た時に発火
-      if (scroll > triggerTop - windowHeight / 0.4){
-        $(this).addClass(animateClass);
-      }
-    });
-  });
+function onIntersect(target, options = {}) {
+  const observer = new IntersectionObserver(this.addShowClass, options)
+  observer.observe(target)
+};
 
-  $('#scroll-btn').click(function() {
-    ('html,body').scrollTop(0);
-  });
-  
-});
+function addShowClass(entries) {
+  for (const e of entries) {
+    if (e.isIntersecting) {
+      e.target.classList.add("is-show")
+    }
+  }
+};
 
